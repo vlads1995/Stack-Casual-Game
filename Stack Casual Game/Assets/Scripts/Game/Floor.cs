d71fe63e709652a54df39645c2c4ea005d83957a
vlads1995 <vlad_s1995@mail.ru>
2019-03-20 21:26:43 +0300

    Initial Commit

---
+++ Stack Casual Game/Assets/Scripts/Game/Floor.cs
@@ -0,0 +1,20 @@
+﻿ using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private int _collisionCount = 0;
+    public static bool isGameOver=false;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Block")
+        {
+            _collisionCount++;
+        }
+
+        if (_collisionCount >= 2)
+        {            
+            isGameOver = true;            
+        }
+    }
+}
---
+++ Stack Casual Game/Assets/Scripts/Game/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    private Vector2 _currentCameraPos;
+    private Vector2 _newCameraPos;
+
+    void Update()
+    {
+        _currentCameraPos = new Vector2(transform.position.x, transform.position.y);
+         
+        if (Controller.moveCamera == true)
+        {
+            _newCameraPos = new Vector2(transform.position.x, transform.position.y + Controller.deltaYPos);
+            Controller.moveCamera = false;           
+        }
+
+        if (_currentCameraPos != _newCameraPos)
+        {
+            Vector2 cameraVelocity = new Vector2(0, 2);            
+            transform.position = Vector2.SmoothDamp(_currentCameraPos, _newCameraPos, ref cameraVelocity, 1);
+        }
+        
+     }
+}
